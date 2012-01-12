f828fcaa7005aa713b9ef2fb7558b4f75e0ee20c
Joshua Vogelstein <joshuav@jhu.edu>
2012-01-12 08:07:05 -0800

    modified setup.py

--- code/setup.py
+++ code/setup.py
@@ -17,7 +17,7 @@ setup(
     name = "PyGraphStats",
     version = "0.1",
     packages = find_packages(),
-    py_modules = ['random', 'embedding', 'classifiers',],
+    py_modules = ['random', 'embedding', 'classifiers','hypothesis-testing'],
     install_requires = ['numpy>=1.3', 'scipy>=0.7', 'matplotlib>=0.99', 'h5py>=1.3', 'multiprocessing>=0.7'],
 
     # metadata for upload to PyPI
