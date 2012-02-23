acb47c97bede1f48aa9ca928732bb17e226f6e2d
Daniel Sussman <dsussman@dsussman-desktop.(none)>
2012-02-23 08:55:19 -0500

    Added the Embed class which is actually kind of nice. Probably will get rid of other stuff later so we can stick with the Embed class/
    Signed-off-by: Daniel Sussman <dsussman@dsussman-desktop.(none)>

--- code/Embed.py
+++ code/Embed.py
@@ -10,6 +10,142 @@ import networkx as nx
 import scipy.sparse as sparse
 from scipy.sparse import linalg as la
 from sklearn.cluster import KMeans
+from itertools import product
+
+def adjacency_matrix(G):
+    return np.array(nx.adjacency_matrix(G))
+adjacency_sparse = nx.to_scipy_sparse_matrix
+
+def laplacian_sparse(G):
+    n = G.number_of_nodes()
+    A = nx.to_scipy_sparse_matrix(G)
+    degree =  A*np.ones(n)
+    scale = sparse.lil_matrix((n,n))
+    scale.setdiag([np.sqrt(1.0/deg) if deg!=0 else 0 for deg in degree])
+    #scale = np.array([np.sqrt(d**-1) if d!=0 else 0 for d in degree])
+    return scale*A*scale
+
+def laplacian_matrix(G):
+    n = G.number_of_nodes()
+    A = np.array(nx.adjacency_matrix(G))
+    degree = np.dot(A,np.ones(n))
+    scale = [np.sqrt(1.0/deg) if deg!=0 else 0 for deg in degree]
+    return np.dot(np.diag(scale),np.dot(A, np.diag(scale)))
+
+class Embed(object):
+    dim = None
+    
+    sval = None
+    svec = None
+    
+    matrix = None
+    G = None
+    
+    def __init__(self, dim, matrix=adjacency_matrix):
+        self.dim = dim
+        self.matrix = matrix
+    
+    
+    def __check_dim(self,d):
+        if d<1:
+            raise ValueError('Dimension must be >=1')
+        if d>self.dim:
+            raise ValueError('Dimension must be <=self.dim')
+        
+    def embed(self, G, fast=True):
+        if self.G is not G or not fast:
+            self.G = G
+            self.svec,self.sval,_ = la.svds(self.matrix(G), self.dim)
+            self.sval = self.sval[::-1]
+            self.svec = self.svec[:, ::-1]
+        
+    def get_embedding(self, d, scale=None):
+        if scale:
+            return self.get_scaled(d)
+        else:
+            return self.get_unscaled(d)
+    
+    def get_unscaled(self, d=None):
+        if not d:
+            d=self.dim
+        
+        self.__check_dim(d)
+          
+        return self.svec[:,np.arange(d)]
+        
+    def get_scaled(self, d=None):
+        if not d:
+            d=self.dim
+        self.__check_dim(d)
+        
+        return np.dot(self.svec[:,np.arange(d)], 
+                    np.diag(np.sqrt(self.sval[np.arange(d)])))
+        
+class EmbedIter(object):
+    d = []
+    matrix = []
+    embed = None
+    
+    def __init__(self, d_range, matrices, scales):
+        self.d = d_range
+        self.matrix = matrices
+        self.scale = scales
+        self.embed = [Embed(np.max(self.d), m) for m in self.matrix]
+        
+        
+    def get_embedding(self, G):
+        for ( d,embed, scale) in product(self.d, self.embed, self.scale):
+            embed.embed(G)
+            yield embed.get_embedding(d, scale)
+        
+        
+    
+
+
+
+#
+#class DotProductEmbed(Embed):
+#    sparse = None
+#    
+#    def __init__(self, dim, sparse=Falscipy_sparse_matrix(se):
+#        self.dim = dim
+#        self.sparse = sparse
+#    
+#    def embed(self, G, scaled=True):
+#        assert(isinstance(G, nx.Graph))
+#        if self.sparse:
+#            A = nx.to_scipy_sparse_matrix(G)
+#        else:
+#            A = nx.adjacency_matrix(G)
+#        self.svecs,self.svals,_ = la.svds(A, self.dim)
+#
+#class LaplacianEmbed(Embed):
+#    sparse = None
+#    
+#    def __init__(self, dim, sparse=False):
+#        self.dim = dim
+#        self.sparse = sparse
+#    
+#    def embed(self, G, scaled=True):
+#        assert(isinstance(G, nx.Graph))
+#        n = G.number_of_nodes()
+#        if self.sparse:
+#            A = nx.to_scipy_sparse_matrix(G)
+#            degree =  A*np.ones(n)
+#            scale = sparse.lil_matrix((n,n))
+#            scale.setdiag([np.sqrt(1.0/deg) if deg!=0 else 0 for deg in degree])
+#            #scale = np.array([np.sqrt(d**-1) if d!=0 else 0 for d in degree])
+#            L = scale*A*scale
+#        else:
+#            A = nx.adjacency_matrix(G)
+#            degree = np.dot(A,np.ones(n))
+#            scale = [np.sqrt(1.0/deg) if deg!=0 else 0 for deg in degree]
+#            L = np.dot(np.diag(scale),np.dot(A, np.diag(scale)))
+#            
+#        self.svecs,self.svals,_ = la.svds(L, self.dim)
+#            
+#            
+    
 
 def dot_product_embed(G, d, scaled=True):
     """ Generates an n by d matrix using an svd of the adjacency matrix
@@ -28,13 +164,15 @@ def dot_product_embed(G, d, scaled=True):
     -------
     n times d matrix where n=G.number_of_nodes()
     """
-    A = nx.to_scipy_sparse_matrix(G)
+    A = adjacency_matrix(G)
     if scaled:
         u,s,_ = la.svds(A, d)
         return np.dot(u,np.diag(np.sqrt(s)))
     else:
         u,_,_ = la.svds(A, d)
         return u
+
+dot_product_embed_unscaled = lambda G,d: dot_product_embed(G,d,scaled=False)
 
 def normalized_laplacian_embed(G,d, scaled=False):
     """ Generates an n by d matrix using an svd of the normalized laplacian
@@ -53,12 +191,24 @@ def normalized_laplacian_embed(G,d, scaled=False):
     -------
     n times d matrix where n=G.number_of_nodes()
     """
-    L = nx.normalized_laplacian(G)
+#    n = G.number_of_nodes()
+#    A = nx.to_scipy_sparse_matrix(G)
+#    degree =  A*np.ones(n)
+#    
+#    scale = sparse.lil_matrix((n,n))
+#    scale.setdiag([np.sqrt(1.0/deg) if deg!=0 else 0 for deg in degree])
+#    
+#    L = scale*A*scale
+    L  = laplacian_matrix(G)
+    
     if scaled:
         u,s,_ = la.svds(sparse.csr_matrix(L), d)
         return np.dot(u,np.diag(np.sqrt(s)))
     else:
         u,_,_ = la.svds(sparse.csr_matrix(L), d)
+        return u
+        
+normalized_laplacian_embed_scaled = lambda G,d:normalized_laplacian_embed(G, d, scaled=True)
         
 def cluster_vertices_kmeans(G, embed, d, k, name=None):
     """ Clusters vertices into k groups based on an embedding in d dimensions
@@ -71,7 +221,7 @@ def cluster_vertices_kmeans(G, embed, d, k, name=None):
     k -- number of clusters
     name -- if not None then assign labels as attribute name to each node
     """
-    k_means = KMeans(init='k-means++', k=3, n_init=10)
+    k_means = KMeans(init='k-means++', k=k, n_init=10)
     x = embed(G, d)
     k_means.fit(x)
     label = k_means.labels_
